f633842c0e5cf93e29f943ca7d4fd88ec41005ae
lab_services_student <lab_services_student@lab7L95SR>
2025-03-05 14:04:13 +0200

    Add project files.

---
+++ ICE Task 1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+
+namespace Program
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Enter your name:");
+
+            String name = Console.ReadLine();
+
+
+
+            Console.WriteLine("Enter your student number:");
+
+            String StudentNumber = Console.ReadLine();
+
+
+
+            Console.WriteLine("Enter your course:");
+
+            String Course = Console.ReadLine();
+
+
+
+            Console.Write("Enter the number of modules your doing:");
+
+            String NumbModules = Console.ReadLine();
+
+
+
+            Console.WriteLine("Your name is " + name + " Your student number is " + StudentNumber + $"Your course is {Course} and your doing {NumbModules} modules");
+
+        }
+
+    }
+
+}
